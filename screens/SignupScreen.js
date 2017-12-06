a4f1d069559c45afebddd3bf746f1aaefe0b73b1
Joseph Dalmasso <me@jodalmasso.com>
2017-12-06 09:27:09 -0500

    First commit

---
+++ screens/SignupScreen.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class SignupScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>SignupScreen</Text>
+        <Text>SignupScreen</Text>
+        <Text>SignupScreen</Text>
+        <Text>SignupScreen</Text>
+        <Text>SignupScreen</Text>
+        <Text>SignupScreen</Text>
+        <Text>SignupScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default SignupScreen;
